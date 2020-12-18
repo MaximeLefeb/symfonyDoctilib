4c0eda77c9de0d935a192ea9c60e39ea6b3e1562
Max Maison <lefebvremaximee@gmail.com>
2020-12-18 15:45:49 +0100

    hash password & login on register succesfuly

--- Doctolib_Project/src/Controller/MainController.php
+++ Doctolib_Project/src/Controller/MainController.php
@@ -10,7 +10,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Security\UserAuthentificatorAuthenticator;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class MainController extends AbstractController {
     /**
@@ -25,15 +28,30 @@ class MainController extends AbstractController {
     /**
      * @Route("/AddPatient", name="AddPatient")
      */
-    public function addPatient(EntityManagerInterface $manager, Request $request) :Response {
+    public function addPatient(EntityManagerInterface $manager, Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UserAuthentificatorAuthenticator $authenticator) :Response {
         $patient = new Patient();
         $form = $this->createForm(PatientType::class, $patient);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             try {
+                $patient->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $patient,
+                        $form->get('password')->getData()
+                    )
+                );
+
                 $manager->persist($patient);
                 $manager->flush(); 
+
+                return $guardHandler->authenticateUserAndHandleSuccess(
+                    $patient,
+                    $request,
+                    $authenticator,
+                    'main'
+                );
+
             } catch (ServiceException $e) {
                 return $this->render('main/index.html.twig', [
                     'error' => $e->getMessage(),
@@ -50,15 +68,29 @@ class MainController extends AbstractController {
      /**
      * @Route("/AddPraticien", name="AddPraticien")
      */
-    public function addPraticien(EntityManagerInterface $manager, Request $request) :Response {
+    public function addPraticien(EntityManagerInterface $manager, Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UserAuthentificatorAuthenticator $authenticator) :Response {
         $praticien = new Praticien();
         $form = $this->createForm(PraticienType::class, $praticien);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             try {
+                $praticien->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $praticien,
+                        $form->get('password')->getData()
+                    )
+                );
+
                 $manager->persist($praticien);
                 $manager->flush(); 
+
+                return $guardHandler->authenticateUserAndHandleSuccess(
+                    $praticien,
+                    $request,
+                    $authenticator,
+                    'main'
+                );
             } catch (ServiceException $e) {
                 return $this->render('main/index.html.twig', [
                     'error' => $e->getMessage(),
